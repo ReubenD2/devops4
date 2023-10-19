5c9981c8b2c77acec25a728c3de99b994b33e8f6
ReubenD2 <studentmalay10@gmail.com>
2023-10-19 23:01:37 +0530

    formula changed

--- applic.py
+++ applic.py
@@ -4,6 +4,6 @@ c = 7
 # calculate the semi-perimeter s = (a + b + c)
 
 # calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.51
 print(f'The area of the triangle is with sides as ({a}, {b}, {c}) is
 {area}')
